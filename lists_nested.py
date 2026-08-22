cea6a30d436f4b8eaa70524d90ef954e5f1b18e9
matthewwoodsdev <matthewwoodsdev@gmail.com>
2026-08-22 21:27:48 +0100

    Add nested lists

---
+++ lists_nested.py
@@ -0,0 +1,10 @@
+# Nested lists
+# Matthew Woods - practice file
+
+house = [["hallway", 11.25],
+         ["kitchen", 18.0],
+         ["bedroom", 10.75]]
+
+print(house)
+print(house[1])
+print(house[1][1]) 
