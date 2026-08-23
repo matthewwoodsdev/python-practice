255cced39dcb5a22ef95a9206fb3289ed47e9113
matthewwoodsdev <matthewwoodsdev@gmail.com>
2026-08-23 22:40:38 +0100

    Add data types
    Add a practice file for data types in Python.

---
+++ types.py
@@ -0,0 +1,12 @@
+# Data Types
+# Matthew Woods - practice file
+
+ingredient_name = "Tomatoes"
+ingredient_quantity = 2
+new_ingredient_quantity = 1.5
+is_in_stock = True
+
+print(type(ingredient_name))
+print(type(ingredient_quantity))
+print(type(new_ingredient_quantity))
+print(type(is_in_stock)) 
