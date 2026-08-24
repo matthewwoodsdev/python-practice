b01de02963630221d0232167919efb8da6f6c382
matthewwoodsdev <matthewwoodsdev@gmail.com>
2026-08-24 22:21:05 +0100

    Add list editing exercises
    Demonstrate changing, adding, and removing items in a list.

---
+++ lists/lists_editing.py
@@ -0,0 +1,12 @@
+# Changing, adding and removing list items
+# Matthew Woods - practice file
+
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+areas[0] = 11.50
+print(areas)
+
+areas.append(15.45)
+print(areas)
+
+areas.remove(20.0)
+print(areas) 
