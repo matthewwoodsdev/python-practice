cae17a7a94ff3d4d8b0903ce4ec1c31198578736
matthewwoodsdev <matthewwoodsdev@gmail.com>
2026-08-30 23:33:18 +0100

    Add string and list methods exercises

---
+++ basics/methods.py
@@ -0,0 +1,23 @@
+# Methods
+# Matthew Woods - practice file
+
+# String methods
+place = "poolhouse"
+
+# upper() - all capitals
+print(place.upper())
+
+# count() - how many times a character appears
+print(place.count("o"))
+
+# replace() - swap one bit of text for another
+print(place.replace("pool", "bath"))
+
+# List methods on strings vs lists
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# index() - position of a value
+print(areas.index(20.0))
+
+# count() - works on lists too
+print(areas.count(14.5))
